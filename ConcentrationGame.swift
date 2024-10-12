92e3cae5201c6432edd77475cc4824b1c8206734
AntonKholin <anton.kholin@me.com>
2024-10-12 23:33:27 +0200

    again

--- ConcentrationGame.swift
+++ ConcentrationGame.swift
@@ -12,17 +12,21 @@ class ConcentrationGame {
     private(set) var cards = [Card]()
     private var indexOfOneAndOnlyFaceUpCard: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            
+            return cards.indices.filter { cards[$0].isFaceUp}.oneAndOnly
+//            return faceUpCardIndices.count == 1 ? faceUpCardIndices.first : nil
+            
+//            var foundIndex: Int?
+//            for index in cards.indices {
+//                if cards[index].isFaceUp {
+//                    if foundIndex == nil {
+//                        foundIndex = index
+//                    } else {
+//                        return nil
+//                    }
+//                }
+//            }
+//            return foundIndex
         }
         set {
             for index in cards.indices {
@@ -52,5 +56,11 @@ class ConcentrationGame {
             cards += [card, card]
 
         }
+    }
+}
+
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first : nil;
     }
 }
